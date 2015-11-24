2f3d395a735824f3a3190579eb528706477b9173
caixukun <2468085704@qq.com>
2015-11-25 06:10:34 +0800

    d

--- bucket.c
+++ bucket.c
@@ -1,3 +1,6 @@
+#define MAX(x, y) ((x) >= (y) ? (x) : (y))
+#define MIN(x, y) ((x) <= (y) ? (x) : (y))
+
 /* We keep buckets in a sorted linked list. A bucket b ranges from
  b->first inclusive up to b->next->first exclusive. */
 static int
@@ -86,9 +89,9 @@ random_node(struct bucket *b){
 /* Return the middle id of a bucket. */
 static int
 bucket_middle(struct bucket *b, unsigned char *id_return){
-	int bit1 = ;
-	int bit2 = ;
-	int bit = ;
+	int bit1 = lowbit(b->first);
+	int bit2 = b->next ? lowbit(b->next->first) : -1;
+	int bit = MAX(bit1, bit2) + 1;
 
 	if(bit>=160){
 		return -1;
@@ -97,4 +100,41 @@ bucket_middle(struct bucket *b, unsigned char *id_return){
 	memcpy(id_return, b->first, 20);
 	id_return[bit/8] |= (0x80>>(bit%8));
 	return 1;
+}
+
+/* Return a random id within a bucket. */
+static int 
+bucket_random(struct bucket *b, unsigned char *id_return){
+	int bit1 = lowbit(b=>first);
+	int bit2 = b->next ? lowbit(b->next->first) : -1;
+	int bit = MAX(bit1, bit2) + 1;
+	int i;
+
+	if(bit >= 160){
+		memcpy(id_return, b->first, 20);
+		return 1;
+	}
+
+	memcpy(id_return, b->first, bit / 8);
+	id_return[bit / 8] = b->first[bit / 8] & (0xFF00 >> (bit % 8));
+	id_return[bit / 8] = random() & 0xFF >> (bit % 8);
+	for(i = bit /8 + 1; i < 20; i++){
+		id_return[i] = random() & 0xFF;
+	}
+	return 1;
+}
+
+/* Insert a new node into a bucket. */
+static struct node*
+insert_node(struct node *node){
+	struct bucket *b = find_bucket(node->id, node->ss.ss_family);
+
+	if(b==NULL){
+		return NULL;
+	}
+
+	node->next = b->nodes;
+	b->nodes = node;
+	b->count++;
+	return node;
 }
--- test.c
+++ test.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 
-/* Determine whether id1 or id2 is closer to ref */
-static int 
-xorcmp(const unsigned char *id1,
-	   const unsigned char *id2,
-	   const unsigned char *ref){
-	int i;
-	for(i=0;i<20;i++){
-		unsigned char xor1, xor2;
-		if(id1[i]==id2[i]){
-			continue;
-		}
-		xor1 = id1[i] ^ ref[i];
-		xor2 = id2[i] ^ ref[i];
-		if(xor1 < xor2){
-			return -1;
-		}else{
-			return 1;
+static int
+lowbit(const unsigned char *id){
+	int i, j;
+	for(i=19;i>=0;i--){
+		if(id[i]!=0){
+			break;
 		}
 	}
-	return 0;
+
+	if(i<0){
+		return -1;
+	}
+
+	for(j=7;j>=0;j--){
+		if((id[i] & (0x80 >> j)) != 0){
+			break;
+		}
+	}
+
+	return 8 * i + j;
 }
 
 int main(){
-	char *id1="abce", *id2="abcd", *ref="efgh";
-	printf("resutl = %d\n", xorcmp(id1, id2, ref));
-
+	unsigned char *a = "1";
+	printf("%x", a[0]);
+//	printf("%x", a[1]);
+//1	printf("%x\n", a[2]);
+	printf("lowbit = %d\n", lowbit(a));
 	return 0;
 }
